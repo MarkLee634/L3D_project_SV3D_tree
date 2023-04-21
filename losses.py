3c898eb751eed5f1864686f6851eb022976c7093
chjohnkim <c.h.johnkim@gmail.com>
2023-04-21 15:49:54 -0400

    EMD and DACD losses

--- losses.py
+++ losses.py
@@ -1,10 +1,11 @@
+import torch
 from pytorch3d.ops import knn_points 
+from Density_aware_Chamfer_Distance.utils_v2.model_utils import calc_dcd, calc_emd
+
 
 
 def chamfer_loss(point_cloud_src,point_cloud_tgt):
 	# point_cloud_src, point_cloud_src: b x n_points x 3  
-	# loss_chamfer = 
-	# implement chamfer loss from scratch
 
 	# Compute the nearest neighbors of the points in point_cloud_src in point_cloud_tgt
 	src_nn = knn_points(point_cloud_src, point_cloud_tgt)
@@ -19,4 +20,10 @@ def chamfer_loss(point_cloud_src,point_cloud_tgt):
 
 	return loss_chamfer.mean()
 
+def density_aware_chamfer_loss(point_cloud_src, point_cloud_tgt):
+	loss, _, _ = calc_dcd(point_cloud_src, point_cloud_tgt)
+	return loss.mean()
 
+def earth_mover_distance(point_cloud_src, point_cloud_tgt):
+	return calc_emd(point_cloud_src, point_cloud_tgt).mean()
+	
